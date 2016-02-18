5d6bc712b4fcce0a5d58964e3c3c62e5dc118fc4
Muhammad Nabeel Arif <nabeel.arif100@gmail.com>
2016-02-18 22:41:59 +0500

    Initial Commit

---
+++ LivePathTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LivePathTracker
+//
+//  Created by Nabeel Arif on 2/18/16.
+//  Copyright © 2016 Nabeel Arif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
